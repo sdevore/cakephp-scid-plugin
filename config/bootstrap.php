dd6038d476f9cbe7abd480a87b49afc7674664fa
Sam DeVore <sdevore@gmail.com>
2018-05-01 15:07:58 -0700

    Update to FontAwesome 5.0.11

---
+++ config/bootstrap.php
@@ -0,0 +1,7 @@
+<?php
+    /**
+     * Created by PhpStorm.
+     * User: sdevore
+     * Date: 4/30/18
+     * Time: 2:46 PM
+     */
--- webroot/fontawesome/svg-with-js/js/fontawesome.js
+++ webroot/fontawesome/svg-with-js/js/fontawesome.js
@@ -1,5 +1,5 @@
 /*!
- * Font Awesome Pro 5.0.9 by @fontawesome - https://fontawesome.com
+ * Font Awesome Pro 5.0.11 by @fontawesome - https://fontawesome.com
  * License - https://fontawesome.com/license (Commercial License)
  */
 (function () {
@@ -606,7 +606,7 @@ function makeLayersTextAbstract(params) {
 
 var noop$2 = function noop() {};
 var p = config.measurePerformance && PERFORMANCE && PERFORMANCE.mark && PERFORMANCE.measure ? PERFORMANCE : { mark: noop$2, measure: noop$2 };
-var preamble = 'FA "5.0.9"';
+var preamble = 'FA "5.0.11"';
 
 var begin = function begin(name) {
   p.mark(preamble + ' ' + name + ' begins');
