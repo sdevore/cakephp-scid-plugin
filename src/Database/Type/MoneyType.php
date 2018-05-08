011832f45636a7a703a219c79fce94f4f7600f3d
Sam DeVore <sdevore@gmail.com>
2018-05-08 11:34:02 -0700

    Start drafting a money datatype

---
+++ src/Database/Type/MoneyType.php
@@ -0,0 +1,18 @@
+<?php
+    /**
+     * Created by PhpStorm.
+     * User: sdevore
+     * Date: 5/8/18
+     * Time: 11:30 AM
+     */
+
+    namespace App\Database\Type;
+
+    use Cake\Database\Driver;
+    use Cake\Database\Type;
+    use PDO;
+
+    class MoneyType
+    {
+
+    }
