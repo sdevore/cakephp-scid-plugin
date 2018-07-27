2be347732cb70d4d8ef1f018ed07e8298277b440
Sam DeVore <sdevore@gmail.com>
2018-07-27 04:24:00 -0700

    payment build out

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -8,6 +8,7 @@ use App\Controller\AppController as BaseController;
  * @property \CakeDC\Users\Controller\Component\RememberMeComponent $RememberMe
  * @property \CakeDC\Users\Controller\Component\UsersAuthComponent $UsersAuth
  * @property \Utils\Controller\Component\GlobalAuthComponent $GlobalAuth
+ * @property \Scid\Controller\Component\PaymentComponent $Payment
  */
 class AppController extends BaseController
 {
