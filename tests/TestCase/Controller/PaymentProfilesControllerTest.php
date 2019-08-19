cbc5412797d773ad4ad15ee52ef9eb67b7fbe056
Sam DeVore <sdevore@gmail.com>
2019-08-19 09:46:17 -0700

    extend some controller properties

---
+++ tests/TestCase/Controller/PaymentProfilesControllerTest.php
@@ -0,0 +1,71 @@
+<?php
+namespace Scid\Test\TestCase\Controller;
+
+use Cake\TestSuite\IntegrationTestCase;
+use Scid\Controller\PaymentProfilesController;
+
+/**
+ * Scid\Controller\PaymentProfilesController Test Case
+ */
+class PaymentProfilesControllerTest extends IntegrationTestCase
+{
+
+    /**
+     * Fixtures
+     *
+     * @var array
+     */
+    public $fixtures = [
+        'plugin.scid.payment_profiles'
+    ];
+
+    /**
+     * Test index method
+     *
+     * @return void
+     */
+    public function testIndex()
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+
+    /**
+     * Test view method
+     *
+     * @return void
+     */
+    public function testView()
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+
+    /**
+     * Test add method
+     *
+     * @return void
+     */
+    public function testAdd()
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+
+    /**
+     * Test edit method
+     *
+     * @return void
+     */
+    public function testEdit()
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+
+    /**
+     * Test delete method
+     *
+     * @return void
+     */
+    public function testDelete()
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+}
