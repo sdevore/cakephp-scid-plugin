4c62b9004f1bf9518e5e19ffcfbecf6cedfc5914
Sam DeVore <sdevore@gmail.com>
2020-01-15 18:43:40 -0700

    Add a phone static function to ScidUtils

--- src/Utility/ScidUtils.php
+++ src/Utility/ScidUtils.php
@@ -340,6 +340,21 @@ class ScidUtils
         return $datetime;
     }
 
+    public
+    static function phone($phone) {
+        $phone = preg_replace("/[^0-9]/", "", $phone);
+
+        if (strlen($phone) == 7) {
+            return preg_replace("/([0-9]{3})([0-9]{4})/", "$1-$2", $phone);
+        }
+        elseif (strlen($phone) == 10) {
+            return preg_replace("/([0-9]{3})([0-9]{3})([0-9]{4})/", "($1) $2-$3", $phone);
+        }
+        else {
+            return $phone;
+        }
+    }
+
     /**
      * @param   \Cake\I18n\Time $time
      * @param array             $format
